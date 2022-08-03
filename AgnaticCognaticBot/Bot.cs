949ac09c299cb8e53918761b80f2d6db15156891
Calco Dev <calcopoddev@gmail.com>
2022-08-03 11:56:56 +0300

    Switched logging from Console.WriteLine() to NLog.

--- AgnaticCognaticBot/Bot.cs
+++ AgnaticCognaticBot/Bot.cs
@@ -2,6 +2,7 @@
 using AgnaticCognaticBot.Services;
 using Discord;
 using Discord.WebSocket;
+using NLog;
 
 namespace AgnaticCognaticBot;
 
@@ -12,11 +13,16 @@ public class Bot
     public readonly ServiceHandler ServiceHandler;
     public readonly CommandHandler CommandHandler;
 
+    private readonly Logger _logger;
+    
     private readonly string _token;
     private bool _running;
 
     public Bot(string token)
     {
+        LogManager.LoadConfiguration("nlog.config.xml");
+        _logger = LogManager.GetCurrentClassLogger();
+        
         _running = true;
         _token = token;
         
@@ -47,7 +53,10 @@ public class Bot
         {
             // Do stuff each "frame"
         }
-
+        
+        _logger.Info("Shutting down logger...");
+        LogManager.Shutdown();
+        
         await Client.LogoutAsync();
         await Client.StopAsync();
     }
@@ -59,7 +68,32 @@ public class Bot
 
     private Task Log(LogMessage logMessage)
     {
-        Console.WriteLine(logMessage.ToString());
+        string message = $"{logMessage.Source}: {logMessage.Message} {logMessage.Exception}";
+        
+        switch (logMessage.Severity)
+        {
+            case LogSeverity.Critical:
+                _logger.Fatal(message);
+                break;
+            case LogSeverity.Error:
+                _logger.Error(message);
+                break;
+            case LogSeverity.Warning:
+                _logger.Warn(message);
+                break;
+            case LogSeverity.Info:
+                _logger.Info(message);
+                break;
+            case LogSeverity.Verbose:
+                break;
+            case LogSeverity.Debug:
+                _logger.Debug(message);
+                break;
+            default:
+                _logger.Warn("Unknown log severity! " + message);
+                break;
+        }
+        
         return Task.CompletedTask;
     }
 }
